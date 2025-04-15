b7ab82c7ac252745250939281f2955453e965240
ArcaneFoxie <arcanefoxie@gmail.com>
2025-04-15 17:51:08 +0100

    finalised mac support

--- compile.sh
+++ compile.sh
@@ -28,11 +28,29 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+echo "Removing code signature (macOS)..."
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    codesign --remove-signature dist/NowSpinnin-
+    if [ $? -ne 0 ]; then
+        echo "Removing code signature failed"
+        exit 1
+    fi
+fi
+
 echo "Injecting blob..."
-npx postject dist/NowSpinnin- NODE_SEA_BLOB dist/sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
+npx postject dist/NowSpinnin- NODE_SEA_BLOB dist/sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 --macho-segment-name NODE_SEA 
 if [ $? -ne 0 ]; then
     echo "Blob injection failed"
     exit 1
+fi
+
+echo "Re-signing binary (macOS)..."
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    codesign --sign - dist/NowSpinnin-
+    if [ $? -ne 0 ]; then
+        echo "Re-signing binary failed"
+        exit 1
+    fi
 fi
 
 echo "Setting executable permissions..."
