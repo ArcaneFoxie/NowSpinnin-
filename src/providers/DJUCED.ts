7a835de3ce788a03d815768aa3eea1a655e0acec
ArcaneFoxie <arcanefoxie@gmail.com>
2025-04-18 00:13:03 +0100

    export DJUCED additionalData

--- src/providers/DJUCED.ts
+++ src/providers/DJUCED.ts
@@ -17,9 +17,20 @@ class DJUCED extends Provider {
   }
 
   async getLatestSong() {
-    const q = this.db.prepare('SELECT artist, title, absolutepath FROM tracks WHERE last_played IS NOT NULL ORDER BY last_played DESC LIMIT 1')
-    const r = q.all()[0] as unknown as Song
-    return r ?? null
+    const q = this.db.prepare('SELECT * FROM tracks WHERE last_played IS NOT NULL ORDER BY last_played DESC LIMIT 1')
+    
+    const data = q.all()[0]
+    if (!data) { return null }
+    
+    delete data.waveform
+
+    return  { 
+      artist: data.artist,
+      title: data.title,
+      absolutepath: data.absolutepath,
+      coverArt: null,
+      additionalData: data
+    } as Song
   }
 
   async dispose() {
